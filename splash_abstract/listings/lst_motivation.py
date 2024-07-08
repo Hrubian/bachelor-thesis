fd0b889d48c5e87e9094582aedfe569f35243c39
janhruby <jan.hruby@second-foundation.eu>
2024-07-08 21:57:15 +0200

    final splash poster

--- splash_abstract/listings/lst_motivation.py
+++ splash_abstract/listings/lst_motivation.py
@@ -3,4 +3,4 @@ df = titanic # reference instead of copy
 titanic.drop("Pclass", inplace=True)
 byclass = df[["Pclass", "Survived"]]\
     .groupby("Pclass").mean() # Pclass undefined
-byclass["Surviwed"] # <- typo
+byclass["Surviwed"] # should be "Survived"
